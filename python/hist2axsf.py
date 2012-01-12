7b9c01a69af0e005c9ffb0644c150f24f4c61594
Kane O'Donnell <kane@as014188.(none)>
2012-01-12 16:35:40 +1100

    Revised to include forces in AXSF output

--- python/hist2axsf.py
+++ python/hist2axsf.py
@@ -5,6 +5,9 @@ from Scientific.IO import NetCDF
 
 def bohr2ang(bohr):
     return bohr * 0.529177249
+    
+def ang2bohr(ang):
+    return ang / 0.529177249
 
 infilename = "/home/kane/Desktop/poro_HIST"
 outfilename = "/home/kane/Desktop/out.axsf"
@@ -31,22 +34,27 @@ print ""
 # Write AXSF header
 outfile.write("ANIMSTEPS %d\n" % steps)
 outfile.write("CRYSTAL\n")
-outfile.write("PRIMVEC\n")
 
-# Get cell lattice vectors and write them
-rprimd = bohr2ang(infile.variables['rprimd'].getValue())
-outfile.write("    %g    %g    %g\n" % (rprimd[0,0,0], rprimd[0,1,0], rprimd[0,2,0]))
-outfile.write("    %g    %g    %g\n" % (rprimd[0,0,1], rprimd[0,1,1], rprimd[0,2,1]))
-outfile.write("    %g    %g    %g\n" % (rprimd[0,0,2], rprimd[0,1,2], rprimd[0,2,2]))
-
-# Now loop over the steps and write coords
+# Get the variables we want to write in the animation
 xcart = bohr2ang(infile.variables['xcart'].getValue())
+rprimd = bohr2ang(infile.variables['rprimd'].getValue())
+fcart = ang2bohr(infile.variables['fcart'].getValue())
 
+# Loop over steps
 for i in range(steps):
+    
+    # Write cell lattice at each step
+    outfile.write("PRIMVEC %d\n" % (i+1))
+    outfile.write("    %g    %g    %g\n" % (rprimd[i,0,0], rprimd[i,1,0], rprimd[i,2,0]))
+    outfile.write("    %g    %g    %g\n" % (rprimd[i,0,1], rprimd[i,1,1], rprimd[i,2,1]))
+    outfile.write("    %g    %g    %g\n" % (rprimd[i,0,2], rprimd[i,1,2], rprimd[i,2,2]))
+    # Write atomic positions and forces at each step
     outfile.write("PRIMCOORD %d\n" % (i+1))
     outfile.write("%d    1\n" % xcart.shape[1])
     for j in range(xcart.shape[1]):
-        outfile.write("%d    %g    %g    %g\n" % (ntypat[j], xcart[i, j, 0], xcart[i, j, 1], xcart[i, j, 2]))
+        outfile.write("%d    %g    %g    %g    %g    %g    %g\n" % (ntypat[j], \
+                        xcart[i, j, 0], xcart[i, j, 1], xcart[i, j, 2], \
+                        fcart[i, j, 0], fcart[i, j, 1], fcart[i, j, 2]))
 
 outfile.write("\n")
 outfile.close()
