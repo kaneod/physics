edb20ce45e0f726acae9dc9bc96db7c541086a51
Kane O'Donnell <kane.odonnell@gmail.com>
2012-01-31 11:11:29 +1100

    First pythonized esc_lib.f90

--- fortran/esc_lib.f90
+++ fortran/esc_lib.f90
@@ -1,30 +1,41 @@
-module esclibf90
+module abilib
 
     implicit none
+    
+    integer, parameter :: dp = selected_real_kind(15)
+    character(6)        :: codvsn
+    integer             :: headform, fform
+        
+    integer             :: bantot, date, intxc, ixc, natom, ngfft(3), nkpt, npsp, &
+                    &   nspden, nspinor, nsppol, nsym, ntypat, occopt, pertcase, &
+                    &   usepaw, usewvl, cplex
+    real(kind=dp)       :: acell(3), ecut, ecutdg, ecutsm, ecut_eff, qptn(3), &
+                    &   rprimd(3,3), stmbias, tphysel, tsmear        
+    integer, allocatable, dimension(:) :: istwfk, nband, npwarr, so_psp, symafm, &
+                    &   typat, nrhoijsel
+    integer, allocatable, dimension(:,:,:) :: symrel
+    integer, allocatable, dimension(:,:) :: rhoijselect
+    real(kind=dp), allocatable, dimension(:) :: occ, znucltypat, wtk
+    character(132)      :: title
+    real(kind=dp)       :: znuclpsp, zionpsp
+    integer             :: pspso, pspdat, pspcod, pspxc, lmax, lloc, mmax
+    real(kind=dp)       :: residm, etotal, fermie
+    real(kind=dp), allocatable, dimension(:,:) :: rhoij, kpt, tnons, xred
+    
+    contains
 
     subroutine read_abinit_density(filename)
         
-        ! Header variables
-        character*6         :: codvsn
-        integer             :: headform, fform
-        integer             :: bantot, date, intxc, ixc, natom, ngfft(3), nkpt, npsp, &
-                        &   nspden, nspinor, nsppol, nsym, ntypat, occopt, pertcase, &
-                        &   usepaw, usewvl, cplex, nspden
-        double precision    :: acell(3), ecut, ecutdg, ecutsm, ecut_eff, qptn(3), &
-                        &   rprimd(3,3), stmbias, tphysel, tsmear
-        integer             :: istwfk(nkpt), nband(nkpt * nsppol), npwarr(nkpt), &
-                        &   so_psp(npsp), symafm(nsym), symrel(3,3,nsym), typat(natom) &
-                        &   nrhoijsel(nspden), rhoijselect(*,nspden)
-        double precision    :: kpt(3,nkpt), occ(bantot), tnons(3,nsym), &
-                        &   znucltypat(ntypat), wtk(nkpt)
-        character*132       :: title
-        double precision    :: znuclpsp, zionpsp
-        integer             :: pspso, pspdat, pspcod, pspxc, lmax, lloc, mmax=integers
-        double precision    :: residm, xred(3, natom), etotal, fermie, rhoij(*, nspden)
+        ! Input variables
+        
+        character(132), intent(in) :: filename
         
         ! Subroutine variables
-        integer             :: status
-        
+        ! (none)
+
+! f2py specifications
+!f2py   intent(in)  filename
+       
         open(unit=100, file=filename, status='old', form='unformatted')
         
         read(100) codvsn, headform, fform
