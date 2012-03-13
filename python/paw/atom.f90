f005724009fc0204c6114debba339cb37e3d2768
Kane O'Donnell <kane.odonnell@gmail.com>
2012-03-13 17:07:58 +1100

    Updates to toy DFT program

--- python/paw/atom.f90
+++ python/paw/atom.f90
@@ -5,7 +5,8 @@ module atom
   
   double precision  :: nuclear_charge, num_electrons
   integer           :: num_states
-  double precision, allocatable, dimension(:) :: eigenvalues, occupancies
+  double precision, allocatable, dimension(:) :: eigenvalues, occupancies, &
+  & density
   double precision, allocatable, dimension(:,:) :: F, v, u
   integer, allocatable, dimension(:,:)           :: quantum_numbers
   
@@ -34,7 +35,8 @@ module atom
     delta = delta_in
     
     ! Init grid
-    if (.not. allocated(grid)) allocate(grid(grid_size))
+    if (allocated(grid)) deallocate(grid)
+    allocate(grid(grid_size))
     rp = rmax / (exp(grid_size * delta) - 1)    
     do j=1,grid_size
       grid(j) = rp * (exp(j * delta) - 1)
@@ -44,7 +46,8 @@ module atom
     n = 1
     l = 0
     !m = 0 ! We aren't dealing with m-numbers in this code yet.
-    if (.not. allocated(quantum_numbers)) allocate(quantum_numbers(num_states,2))
+    if (allocated(quantum_numbers)) deallocate(quantum_numbers)
+    allocate(quantum_numbers(num_states,2))
     do i=1,num_states
       quantum_numbers(i,1) = n
       quantum_numbers(i,2) = l
@@ -90,65 +93,57 @@ module atom
     
     do i=1,num_states
       do j=1,grid_size
+        u(i,j) = 0.0d0
+        v(i,j) = 0.0d0
         F(i,j) = (rp ** 2) * (delta ** 2) * exp(2 * j * delta) * &
         & numerov_f(quantum_numbers(i,2), grid(j), eigenvalues(i)) +  &
         & 0.25d0 * (delta ** 2)
       end do
     end do
+    
+    ! Generate the initial states
+    do i=1,num_states
+      call numerov(i,"backwards")
+    end do
+    
+    ! Generate the initial density
+    call compute_density
   
   end subroutine
+  
+  subroutine compute_density
+  
+    integer :: i, j
+    
+    if (allocated(density)) deallocate(density)
+    allocate(density(grid_size))
+    
+    ! Ensure density is zero
+    do j=1,grid_size
+      density(j) = 0.0d0
+    end do
+    
+    do i=1,num_states
+      do j=1,grid_size
+        density(j) = density(j) + occupancies(i) * u(i,j) * u(i,j)
+      end do
+    end do
+    
+  end subroutine
+    
   
   subroutine numerov(state_index, direction)
   
     integer :: j, state_index
     character(len=8) :: direction
-    double precision, allocatable, dimension(:) :: A, B, C
-    
-    allocate(A(grid_size))
-    allocate(B(grid_size))
-    allocate(C(grid_size))
-    
-    do j=1,grid_size
-      A(j) = 2.0d0 + (5.0d0 / 6.0d0) * F(state_index, j)
-      B(j) = 1.0d0 - (1.0d0 / 12.0d0) * F(state_index, j)
-      C(j) = 1.0d0 / B(j)
-    end do
+    double precision :: q0, q1, q2, unorm
     
     select case(trim(direction))
     case('forward')
       v(state_index,1) = 0.0d0
       v(state_index,2) = 1.0d0
-      do j=1,(grid_size-2)
-        v(state_index,j+2) = (v(state_index,j+1) * A(j+1) - v(state_index,j) * B(j)) * C(j+2)
-      end do
-    case('backward')
-      v(state_index, grid_size) = 0.0d0
-      v(state_index, grid_size-1) = 1.0d0
-      do j=grid_size,3,-1
-        v(state_index,j-2) = (v(state_index,j-1) * A(j-1) - v(state_index,j) * B(j)) * C(j-2)
-      end do
-    end select
-    
-    do j=1,grid_size
-      u(state_index,j) = v(state_index,j) * exp(j * delta)
-    end do
-    
-    deallocate(A)
-    deallocate(B)
-    deallocate(C)
-  
-  end subroutine
-  
-  subroutine numerov2(state_index, direction)
-  
-    integer :: j, state_index
-    character(len=8) :: direction
-    
-    select case(trim(direction))
-    case('forward')
-      v(state_index,1) = 0.0d0
-      v(state_index,2) = 1.0d0
-      q0 = v(state_index,
+      q0 = v(state_index,1) * (1.0d0 - F(state_index,1) / 12.0d0)
+      q1 = v(state_index,2) * (1.0d0 - F(state_index,2) / 12.0d0)
       do j=2,grid_size
         q2 = F(state_index, j-1) * v(state_index, j-1) + 2.0d0 * q1 - q0
         q0 = q1
@@ -156,10 +151,19 @@ module atom
         v(state_index, j) = q1 / (1.0d0 - F(state_index, j))
       end do
     case('backward')
-      v(state_index, grid_size) = 0.0d0
-      v(state_index, grid_size-1) = 1.0d0
-      do j=grid_size,3,-1
-        v(state_index,j-2) = (v(state_index,j-1) * A(j-1) - v(state_index,j) * B(j)) * C(j-2)
+      v(state_index,grid_size) = exp(-1.0d0 * sqrt(-2.0d0 * & 
+      & eigenvalues(state_index)) * grid(grid_size))
+      v(state_index,grid_size-1) = exp(-1.0d0 * sqrt(-2.0d0 * &
+      & eigenvalues(state_index)) * grid(grid_size-1))
+      q0 = v(state_index,grid_size) * (1.0d0 - F(state_index,grid_size) / &
+      & 12.0d0)
+      q1 = v(state_index,grid_size-1) * (1.0d0 - F(state_index,grid_size-1) / &
+      & 12.0d0)
+      do j=grid_size-2,2,-1
+        q2 = F(state_index,j+1) * v(state_index,j+1) + 2.0d0 * q1 - q0
+        q0 = q1
+        q1 = q2
+        v(state_index, j) = q1 / (1.0d0 - F(state_index, j))
       end do
     end select
     
@@ -167,9 +171,9 @@ module atom
       u(state_index,j) = v(state_index,j) * exp(j * delta)
     end do
     
-    deallocate(A)
-    deallocate(B)
-    deallocate(C)
+    unorm = integrate(grid, u(state_index), grid_size)
+    print *, unorm
+    u(state_index) = u(state_index) / unorm
   
   end subroutine   
       
@@ -189,6 +193,22 @@ module atom
     
     ! Just the nuclear potential for now!
     potential = -1.0 * nuclear_charge / r
+    
+  end function
+  
+  double precision function integrate(x, y, n)
+    
+    integer :: n, j
+    double precision :: x(n), y(n)
+    double precision :: psum = 0.0d0
+      
+    do j=1,n-1
+      psum = psum + 0.5 * (x(j+1) - x(j)) * (y(j+1) + y(j))
+    end do
+    
+    integrate = psum
+    
+    return
     
   end function
   
