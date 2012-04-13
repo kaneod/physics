10476cbdd5b0e6ad0e93094df085ea931f5785a3
Kane O'Donnell <kane.odonnell@gmail.com>
2012-04-13 12:28:23 +1000

    More multipole stuff

--- fortran/multipole.f90
+++ fortran/multipole.f90
@@ -1,16 +1,21 @@
 program multipole
-  
+
   implicit none
+  
+  double precision, external :: ddot
+  external :: zgemm, dpotrf, zpotrs, zgemv
   
   ! Counters etc
   integer :: i, p, j, k
+  double precision :: vec(3)
+  double complex :: fsum
   
   ! Timing information
   integer :: clock_start, clock_end, clock_rate
   double precision :: elapsed_time
   
   ! 3D grid
-  integer :: L=50, M=50, N=50
+  integer :: gridx=50, gridy=50, gridz=50
   double complex :: grid(50,50,50)
   
   ! Lattice vectors
@@ -19,9 +24,16 @@ program multipole
   double precision :: r0(3)
   
   ! Points inside computational shell and the max number of basis functions
-  integer :: ns, nmax=5
-  double complex, allocatable, dimension(:) :: S
-  double precision, allocatable, dimension(:) :: rx, ry, rz, rp, Plm
+  ! M is the number of independent {n,l,m} combinations
+  integer :: N, nmax=3, lmax=3, M
+  integer, allocatable, dimension(:,:) :: lm ! Set of l,m combinations
+  integer, allocatable, dimension(:) :: ipiv
+  double complex, allocatable, dimension(:) :: S, F
+  ! Matrices. Need work matrices. Gc is a complex version of G, likewise for Wc.
+  double complex, allocatable, dimension(:,:) :: Y, Wk1, Wk2, Pr, Gc, W
+  ! Real matrices
+  double precision, allocatable, dimension(:,:) :: G, WkL1, WrkL2
+  double precision, allocatable, dimension(:) :: rx, ry, rz, rp
   integer, allocatable, dimension(:) :: px, py, pz
   double precision :: rs, delta ! Radius of sphere, shell width
   
@@ -41,10 +53,7 @@ program multipole
   h7 = 0.125d0 * sqrt(35.0d0 / pi)
   h8 = 0.25d0 * sqrt(105.0d0 / (2.0d0 * pi))
   h9 = 0.125d0 * sqrt(21.0d0 / pi)
-  h10 = 0.25d0 * sqrt(7.0d0 / pi)
-  
-  rs = 1.5
-  delta = 0.2
+  h10 = 0.25d0 * sqrt(7.0d0 / pi)    
   
   c0 = (0.1, 0.9)
   c1 = (0.25, 0.75)
@@ -57,7 +66,19 @@ program multipole
   avec(:,2) = (/ 0.0d0, 10.0d0, 0.0d0 /)
   avec(:,3) = (/ 0.0d0, 0.0d0, 10.0d0 /)
   
-  vol = (0.2d0)**3 ! grid cell volume
+  ! Grid cell volume
+  vec(1) = avec(2,2) * avec(3,3) - avec(3,2) * avec(2,3)
+  vec(2) = avec(1,3) * avec(3,2) - avec(1,2) * avec(3,3)
+  vec(3) = avec(1,2) * avec(2,3) - avec(2,2) * avec(1,3)  
+  vol = ddot(3, avec(:,1), 1, vec, 1) / (gridx * gridy * gridz)
+  
+  rs = 1.5
+  ! Use the suggested value of Fiske (delta = 5/4 * h, or Delta=0.75h)
+  delta = 1.25 * vol ** (real(1)/3)
+  
+  print *, "Cell volume: ", vol * gridx * gridy * gridz
+  print *, "Grid cell volume: ", vol
+  print *, "delta ", delta
   
   r0 = (/ 5.0d0, 5.0d0, 5.0d0 /)
   
@@ -90,25 +111,16 @@ program multipole
   &               real(clock_end - clock_start) / clock_rate  
   
   ! Print out the points list
-  !print *, "Found ", ns, " points inside sphere."
+  print *, "Points in total:", gridx*gridy*gridz
+  print *, "Found ", N, " points inside sphere."
   !print *, "p S(p) px(p) py(p) pz(p) rx(p) ry(p) rz(p) rp(p)"
-  !do i=1,ns
+  !do i=1,N
   !  print '(1i6, 2f5.2, 3i3, 4f5.2)', i, S(i), px(i), py(i), pz(i), rx(i), & 
   !  &                                 ry(i), rz(i), rp(i)
   !end do
   
-  ! Now have a first rough attempt at getting the coefficients (don't worry about
-  ! boundary integration problems)
-  
-  do i=0,3
-    do j=0,0
-      do p=1,ns
-        u(i) = u(i) + Plm(p) * conjg(ylm((/ rx(p)-r0(1), ry(p) - r0(2), rz(p)-r0(3)/), &
-        &                                 i,j)) * S(p) * vol
-      end do
-      print *, "Coefficient for l,m = ", i, j, " is ", u(i)
-    end do
-  end do
+  ! Print out linear combinations of entries in F
+  print *, F
       
   contains
   
@@ -119,10 +131,10 @@ program multipole
     
     ! Note: in future, avoid function calls by using the x,y,z form of the Ylms.
     
-    do k=1,N
-      do j=1,M
-        do i=1,L
-          g = real(i-1)/L * avec(:,1) + real(j-1)/M * avec(:,2) + real(k-1)/N * avec(:,3)
+    do k=1,gridz
+      do j=1,gridy
+        do i=1,gridx
+          g = real(i-1)/gridx * avec(:,1) + real(j-1)/gridy * avec(:,2) + real(k-1)/gridz * avec(:,3)
           r = g - r0
           rho = sqrt(r(1) ** 2 + r(2) ** 2 + r(3) ** 2)
           if (rho .lt. 1.0d-8) then
@@ -140,14 +152,40 @@ program multipole
   subroutine shell_init
     
     integer :: i, j, k, p
-    double precision :: g(3), r(3), rho
+    double precision :: gv(3), r(3), rho
+    
+    ! Figure out how many lm components we need, generate and store.
+    p = 0
+    do i=0,lmax
+      do j=-i,i
+        do k=0,nmax
+          p=p+1
+        end do
+      end do
+    end do
+    M = p
+    
+    if (allocated(lm)) deallocate(lm)
+    allocate(lm(p,3))
+    
+    p = 1
+    do i=0,lmax
+      do j=-i,i
+        do k=0,nmax
+          lm(p,1) = k
+          lm(p,2) = i
+          lm(p,3) = j
+          p = p + 1
+        end do
+      end do
+    end do  
     
     p = 0
-    do k=1,N
-      do j=1,M
-        do i=1,L
-          g = real(i-1)/L * avec(:,1) + real(j-1)/M * avec(:,2) + real(k-1)/N * avec(:,3)
-          r = g - r0
+    do k=1,gridz
+      do j=1,gridy
+        do i=1,gridx
+          gv = real(i-1)/gridx * avec(:,1) + real(j-1)/gridy * avec(:,2) + real(k-1)/gridz * avec(:,3)
+          r = gv - r0
           rho = sqrt(r(1) ** 2 + r(2) ** 2 + r(3) ** 2)     
           if ((rho .ge. rs - delta) .and. (rho .le. rs + delta)) then
             p = p + 1
@@ -156,7 +194,7 @@ program multipole
       end do
     end do
     
-    ns = p
+    N = p
     if (allocated(S)) deallocate(S)
     if (allocated(px)) deallocate(px)
     if (allocated(py)) deallocate(py)
@@ -165,24 +203,33 @@ program multipole
     if (allocated(ry)) deallocate(ry)
     if (allocated(rz)) deallocate(rz)
     if (allocated(rp)) deallocate(rp)
-    if (allocated(Plm)) deallocate(Plm)
-    allocate(S(p), Plm(p), px(p), py(p), pz(p), rx(p), ry(p), rz(p), rp(p))
+    if (allocated(Y)) deallocate(Y)
+    if (allocated(W)) deallocate(W)
+    if (allocated(G)) deallocate(G)
+    if (allocated(Gc)) deallocate(Gc)
+    if (allocated(Wk1)) deallocate(Wk1)
+    if (allocated(Wk2)) deallocate(Wk2)
+    if (allocated(Pr)) deallocate(Pr)
+    if (allocated(F)) deallocate(F)
+    allocate(S(p), px(p), py(p), pz(p), rx(p), ry(p), rz(p), rp(p))
+    allocate(Y(N,M), W(N,N), G(M,M), Wk1(N, M), F(M), Pr(M,N), Wk2(M,N))
+    allocate(Gc(M,M))
     
     p = 1
-    do k=1,N
-      do j=1,M
-        do i=1,L
-          g = real(i-1)/L * avec(:,1) + real(j-1)/M * avec(:,2) + real(k-1)/N * avec(:,3)
-          r = g - r0
+    do k=1,gridz
+      do j=1,gridy
+        do i=1,gridx
+          gv = real(i-1)/gridx * avec(:,1) + real(j-1)/gridy * avec(:,2) + real(k-1)/gridz * avec(:,3)
+          r = gv - r0
           rho = sqrt(r(1) ** 2 + r(2) ** 2 + r(3) ** 2)          
           if ((rho .ge. (rs - delta)) .and. (rho .le. (rs + delta))) then
             S(p) = grid(i,j,k)
             px(p) = i
             py(p) = j
             pz(p) = k
-            rx(p) = g(1)
-            ry(p) = g(2)
-            rz(p) = g(3)
+            rx(p) = gv(1)
+            ry(p) = gv(2)
+            rz(p) = gv(3)
             rp(p) = rho
             p = p + 1
           end if
@@ -190,15 +237,61 @@ program multipole
       end do
     end do
     
-    ! Now generate the prefactors to the Plm projectors.
-    ! It's too bloody hard to pre-generate the projectors themselves for the moment.
-    Plm = 0.0d0
-    do p=1,ns
-      do i=0,nmax
-        Plm(p) = Plm(p) + rlegendre(i, rs) * rlegendre(i,rp(p))
+    ! Generate the weights matrix W
+    W = 0.0d0
+    do p=1,N
+      if (abs(rp(p) - rs) .gt. delta) then
+        W(p,p) = 0.0d0
+      else if (abs(rp(p) - rs) .lt. (delta - (vol ** (real(1)/3)))) then
+        W(p,p) = vol
+      else
+        W(p,p) = (delta - abs(rp(p) - rs)) * (vol ** (real(2)/3))
+      end if
+      if (real(W(p,p)) .lt. 0) then
+        print *, "Warning: Negative weights at p, W(p,p) = ", p, W(p,p)
+      end if
+    end do
+    
+    ! Generate the Y matrix
+    Y = 0.0d0
+    do p=1,N
+      do i=1,M
+        Y(p,i) = rlegendre(lm(i,1), rp(p)) * ylm(rp(p), lm(i,2), lm(i,3))
+        !print *, "p, i, lm(i,:),rp(p), rleg, ylm", p, i, lm(i,:), &
+        !&         rp(p), rlegendre(lm(i,1), rp(p)), ylm(rp(p), lm(i,2), lm(i,3))
       end do
     end do
-        
+    
+    ! Now generate YtW and Gc = YtWY
+    Gc = 0.0d0
+    Wk1 = 0.0d0
+    Wk2 = 0.0d0
+    ! This ZGEMM gives us YtW
+    call zgemm("C", "N", M, N, N, 1.0d0, Y, N, W, N, 0.0d0, Wk2, M)
+    ! This ZGEMM gives Gc = YtWY
+    call zgemm("N", "N", M, M, N, 1.0d0, Wk2, M, Y, N, 0.0d0, Gc, M)
+    
+    deallocate(Wk1)
+    
+    ! Cholesky factorize G = L*Lt, we get G transformed into L on return.
+    !call zpotrf("L", M, Gc, M, i)
+    !print *, "ZPOTRF returned ", i
+    
+    ! Now solve LLtP = YtW
+    ! Remember YtW = Wk2. Need a MxN workspace.
+    allocate(Wk1(M,N))
+    
+    !call zpotrs("L", M, N, Gc, M, Wk2, M, i)
+    allocate(ipiv(M))
+    call zgesv(M, N, Gc, M, ipiv, Wk2, M, i)
+    
+    print *, "ZGESV returned ", i
+    
+    ! Finally, compute the coefficients vector F = PS
+    ! P is stored in Wk2
+    
+    call zgemv("N", M, N, 1.0d0, Wk2, M, S, 1, 0.0d0, F, 1) 
+    
   end subroutine
   
   double complex function ylm(r, l, m)
@@ -263,6 +356,7 @@ program multipole
   
   double precision function legendre(n, x)
   
+    ! There may be a problem with the n=4 and 5 functions here.
     integer, intent(in) :: n
     double precision, intent(in) :: x
     
