4f62425620c25ebe082b1d2eb15731ef74721862
Kane O'Donnell <kane.odonnell@gmail.com>
2012-07-03 23:47:06 +1000

    Shirley and pre-edge background fitting.

--- python/pes_utils.py
+++ python/pes_utils.py
@@ -1,89 +1,122 @@
 
 import numpy as np
 
-tol = 1.0e-6
-HUGE = 1e10
+tol = 1e-6
 
-def shirley_integral(E,F,eidx):
+def preedge_calculate(x,y):
+  """ P = pes_utils.preedge_calculate(x,y)
   
-  # I(E) = int_{E}^{Emax} F(E') dE'
+  Calculates the best-fit linear pre-edge for a dataset (x,y). Finds the biggest peak,
+  then finds the pre-edge region using a sequence of linear fits starting from the end
+  point.
   
-  # Here "Emax" is the smallest binding energy,
-  # so we want the energy scale to be *decreasing*. 
-  
-  Isum = 0.0
-  
-  for i in range(eidx,len(E)-1):
-    Isum += F[i]
-    
-
-def shirley_calculate(x,y,emin):
+  """
 
   # First ensure the energy values are *decreasing* in the array,
   # if not, reverse them.
   
   if x[0] < x[-1]:
+    print "Found x,y arrays badly ordered: Reversing order!"
     E = x[::-1]
     J = y[::-1]
+    
   else:
     E = x
     J = y
     
-  # Figure out the index closest to emin
-  emidx = 0
-  edist = HUGE
-  for i,energy in enumerate(E):
-    if abs(energy - emin) < edist:
-      edist = abs(energy - emin)
-      emidx = i
+  # Locate the biggest peak.
+  maxidx = np.abs(y - np.amax(y)).argmin()
   
+  # Find the gradient of every possible linear fit between the lowest binding energy
+  # and the biggest peak.
+  grads = []
+  for i in range(2, len(x) - maxidx):
+    
+    # Best linear fit to the last i values
+    xs = x[-i:]
+    ys = y[-i:]
+    p = np.polyfit(xs,ys,1)
+    grads.append(p[0])
+    
+  # Differentiate the gradient array.
+  dgrads = []
+  for i in range(len(grads)-1):
+    dgrads.append(grads[i+1] - grads[i])
+  
+  dgrads = np.array(dgrads)
+  
+  # Find the minimum index of the absolute of the gradient of gradients.
+  mingrad = np.abs(dgrads).argmin()
+  
+  # Make a best linear fit from this number of pre-edge points, generate linear
+  # pre-edge.
+  p = np.polyfit(x[-mingrad:], y[-mingrad:], 1)
+  return np.polyval(p,x)
+def shirley_calculate(x,y):
+  """ S = pes_utils.shirley_calculate(x,y)
+  
+  Calculate the best auto-Shirley background S for a dataset (x,y). Finds the biggest peak 
+  and then uses the minimum value either side of this peak as the terminal points of the
+  Shirley background.
+  """
+  
+  # First ensure the energy values are *decreasing* in the array,
+  # if not, reverse them.
+  
+  if x[0] < x[-1]:
+    print "Found x,y arrays badly ordered: Reversing order!"
+    E = x[::-1]
+    J = y[::-1]
+    
+  else:
+    E = x
+    J = y
+    
+  # Locate the biggest peak.
+  maxidx = np.abs(y - np.amax(y)).argmin()
+  
+  # Locate the minima either side of maxidx.
+  lmidx = np.abs(y[0:maxidx] - np.amin(y[0:maxidx])).argmin()
+  rmidx = np.abs(y[maxidx:] - np.amin(y[maxidx:])).argmin() + maxidx
   # Get Eleft, Eright, Ileft, Iright
-  Eleft = E[emidx]
-  Ileft = J[emidx]
-  Eright = E[-1]
-  Iright = J[-1]
+  Eleft = E[lmidx]
+  Ileft = J[lmidx]
+  Eright = E[rmidx]
+  Iright = J[rmidx]
   
   # Max integration index
-  imax = len(E) - 1
+  imax = rmidx - 1
 
   # Initial value of B. The total background S = Iright + B,
-  # and B is equal to Ileft - Iright below emin and initially
+  # and B is equal to (Ileft - Iright) below lmidx and initially
   # zero above.
-  B = zeros(E.shape)
-  B[:emidx] = Ileft - Iright
-  Bnew = B
-  
-  Bs = []
+  B = np.zeros(E.shape)
+  B[:lmidx] = Ileft - Iright
+  Bnew = B.copy()
   
   while True:
     
     # Calculate new kn = (Ileft - Iright) / (int_(Eleft)^(Eright) J(E') - Iright - Bn-1(E') dE')
     ksum = 0.0
-    for i in range(emidx,imax):
+    for i in range(lmidx,imax):
       ksum += (E[i] - E[i+1]) * 0.5 * (J[i] + J[i+1] - 2 * Iright - B[i] - B[i+1])
     k = (Ileft - Iright) / ksum
     
-    print "Found new k: ", k
-    
     # Calculate new B
-    for i in range(emidx,len(E)):
+    for i in range(lmidx,rmidx):
       Isum = 0.0
       for j in range(i,imax):
-        Isum += (E[i] - E[i+1]) * 0.5 * (J[i] + J[i+1] - 2 * Iright - B[i] - B[i+1])
-      print "Calculated isum for E: ", E[i], Isum
+        Isum += (E[j] - E[j+1]) * 0.5 * (J[j] + J[j+1] - 2 * Iright - B[j] - B[j+1])
       Bnew[i] = k * Isum
       
       
     # If Bnew is close to B, exit.
-    print "Bnew difference: ", np.linalg.norm(Bnew - B)
     if np.linalg.norm(Bnew - B) < tol:
-      Bs.append(Bnew)
-      B = Bnew
+      B = Bnew.copy()
       break
     else:
-      Bs.append(Bnew)
-      B = Bnew
+      B = Bnew.copy()
       
-  return Iright, Bs
+  return Iright + B
 
     
