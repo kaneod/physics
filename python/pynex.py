6d9db14ef83c619ed8bd57d94ef0b8935417dd37
Kane O'Donnell <kane.odonnell@gmail.com>
2012-11-01 12:21:53 +1100

    Now generates both separate and combined spectra.
    Can use transition energies file for edge positioning.

--- python/pynex.py
+++ python/pynex.py
@@ -44,17 +44,17 @@
 
 import argparse
 import libpytep as lp
-from numpy import zeros, savetxt
+from numpy import zeros, savetxt, amin, amax, arange, array
+from scipy.interpolate import interp1d
 
 lpe = lp.core_level_spectra
 
-parser = argparse.ArgumentParser(description="Combine individual NEXAFS spectra from different core-hole sites.")
+parser = argparse.ArgumentParser(description="Calculate individual NEXAFS spectra from different core-hole sites.")
 
 parser.add_argument('seed', help="CASTEP base seedname")
 parser.add_argument('atlist', help="List of atomic suffixes.")
 parser.add_argument('atom', help="Atom number of core hole site.")
 parser.add_argument('-t', '--transition_file', default=None, help="Transition energies file.")
-parser.add_argument('-o', '--output', default="single", help="Output single files or combined.")
 
 args = parser.parse_args()
 
@@ -77,6 +77,7 @@ species = int(args.atom)
 spectra = []
 
 for i, a in enumerate(atlist):
+  print "Processing seed: ", prefix+ "_" + str(a)
   seed = prefix + "_" + str(a)
   lpe.init(seed)
   # Set spectral preferences here!
@@ -85,17 +86,72 @@ for i, a in enumerate(atlist):
   lpe.linear_broadening = 0.1
   lpe.generate_single_spectrum(species)
   if transitions is not None:
-    spectra.append([lpe.w.copy() + transitions[i], lpe.lastspec.copy()])
+    # Transition energies tend to be negative (sign convention) so have to
+    # subtract here.
+    spectra.append([lpe.w.copy() - transitions[i], lpe.lastspec.copy()])
   else:
     spectra.append([lpe.w.copy(), lpe.lastspec.copy()])
 
-if args.output == "single":
-  for i, xy in enumerate(spectra):
+# Output the single atomic spectra
+for i, xy in enumerate(spectra):
+  x = xy[0]
+  y = xy[1]
+  tmp = zeros((y.shape[0], y.shape[1]+1))
+  tmp[:,0] = x
+  tmp[:,1:] = y
+  savetxt(prefix+ "_" + str(atlist[i]) + ".nexafs", tmp)
+
+# Combine the individual spectra. This is non-trivial in general because the
+# different transition energies make the x-axis different. So we divide into 
+# two cases:
+#
+# 1. We aren't using transition energies. In this case, just add the spectra 
+#    together pointwise because they are all on the same x-scale. Note that
+#    this is totally invalid in a core-hole calculation with different core-hole
+#    sites.
+#
+# 2. We're using transition energies. Now what we do is figure out the step size
+#    of the spectra (will all be the same) and the global minimum and maximum
+#    energy, then generate a linspace over that range with the same step size. 
+#    In this way the point density of the original spectra stays intact, but it
+#    means the number of points in the global spectrum is not the same as the
+#    individual spectra.
+
+if transitions is None:
+  print "Generating combined output with no transition energies."
+  tmp = zeros((spectra[0][1].shape[0], spectra[0][1].shape[1] + 1))
+  for xy in spectra:
     x = xy[0]
     y = xy[1]
-    tmp = zeros((y.shape[0], y.shape[1]+1))
-    tmp[:,0] = x
-    tmp[:,1:] = y
-    savetxt(prefix+ "_" + atlist[i] + ".nexafs", tmp)
-
-  
+    tmp[:,1:] += y
+  tmp[:,0] = spectra[0][0]
+  savetxt(prefix + "_combined.nexafs", tmp)
+else:
+  # Get the step size
+  step = spectra[0][0][1] - spectra[0][0][0]
+  # Find global min and max energy.
+  minx = None
+  maxx = None
+  for xy in spectra:
+    if minx is None:
+      minx = amin(xy[0])
+    elif amin(xy[0]) < minx:
+      minx = amin(xy[0])
+    if maxx is None:
+      maxx = amax(xy[0])
+    elif amax(xy[0]) > maxx:
+      maxx = amax(xy[0])
+  # Generate a spectrum between minx and maxx with the given step. We want to
+  # include the endpoint here so we have to add a step.
+  x = arange(minx, maxx+step, step)
+  print "Generating combined output on xaxis starting at ", minx, ", stopping at ", maxx, " and with step size ", step, "."
+  print "This gives ", len(x), " points in the spectrum."
+  # Now generate interpolators and interpolate 
+  tmp = zeros((len(x), 7))
+  for xy in spectra:
+    for i in range(6):
+      ixy = interp1d(xy[0], xy[1][:,i], bounds_error=False, fill_value=0.0)
+      tmp[:,i+1] += array([ixy(E) for E in x])
+  tmp[:,0] = x
+  savetxt(prefix + "_combined.nexafs", tmp)
+    
