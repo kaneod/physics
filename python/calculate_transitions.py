12c96e4bc0c16da4db8c23427552aea9d5bafe36
Kane O'Donnell <kane.odonnell@gmail.com>
2013-03-16 12:25:53 +1100

    calculate_transitions.py supports transitions.sh
    The python script does the maths for the method, that's
    all. I just couldn't be bothered doing the maths in BASH
    because the punctuation gives me a headache. :o)

---
+++ python/calculate_transitions.py
@@ -0,0 +1,28 @@
+
+import sys
+from numpy import loadtxt, array, savetxt
+
+total_energies = loadtxt(sys.argv[1])
+neutral_energy = loadtxt(sys.argv[2])
+atom_energies = loadtxt(sys.argv[3])
+
+Eae = atom_energies[0]
+Eaestar = atom_energies[1]
+Eps = atom_energies[2]
+Epsstar = atom_energies[3]
+
+Et = neutral_energy
+
+dEaea = Eaestar - Eae
+dEpsa = Epsstar - Eps
+dEca = dEaea - dEpsa
+
+transitions = []
+
+for Etstar in total_energies:
+  dEv = Etstar - Et
+  transitions.append(dEv + dEca)
+
+transitions=array(transitions)
+
+savetxt("transition_energies.txt", transitions, fmt='%.6f')
