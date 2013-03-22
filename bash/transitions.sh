293645c21acbe8736d2bde261b87687302854b8d
Kane O'Donnell <kane.odonnell@gmail.com>
2013-03-22 16:28:30 +1100

    transitions.sh now outputs spin, nelec.
    pynex.py now requires more input variables, transitions.sh
    has been modified to extract them from the same starting files
    and generatethe necessary input files.

--- bash/transitions.sh
+++ bash/transitions.sh
@@ -3,6 +3,9 @@
 # Extracts the total energies from each of the core hole calculations and puts them
 # in a single file, does the same for the neutral calculation, then runs the python
 # script that calculates the transition energies (written to transitions.txt). 
+# In addition, the script extracts the number of electrons from the NEUTRAL calculation
+# and the spin polarization of the neutral and excited calculations. These are
+# used to figure out where the lowest transition is in the pynex code.
 # Inputs are the CASTEP seed name and the name of a file containing the unique identifiers
 # for each of the core hole calculations, so that the core hole seeds are constructed
 # as ${seed}_${identifier} for each identifier. Normally I just give the identifiers numbers
@@ -23,10 +26,20 @@ if [ -e total_energies.txt ]
 then
   rm total_energies.txt
 fi
+if [ -e total_spins.txt ]
+then
+  rm total_spins.txt
+fi
+if [ -e nelectrons.txt ]
+then
+  rm nelectrons.txt
+fi
 
 for i in `cat $atlist`
 do
   cat ${seed}_${i}.castep | grep "Final energy," | awk '{print $5}' >> total_energies.txt
+  cat ${seed}_${i}.castep | grep "2*Integrated Spin" | awk '{print $5}' >> total_spins.txt
+  cat ${seed}_${i}.castep | grep "number of  electrons" | awk '{print $5}' >> nelectrons.txt
 done
 
 if [ -e ${seed}_neutral.castep ]
@@ -37,6 +50,8 @@ then
   fi
 
   cat ${seed}_neutral.castep | grep "Final energy," | awk '{print $5}' >> neutral.txt
+  cat ${seed}_neutral.castep | grep "2*Integrated Spin" | awk '{print $5}' >> neutral.txt
+  cat ${seed}_neutral.castep | grep "number of  electrons" | awk '{print $5}' >> neutral.txt
 else
   echo "Missing neutral castep file: See the transitions.sh file for details. Cannot continue."
   exit
