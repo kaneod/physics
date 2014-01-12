7afb98f6f67b411c87ae94961021a247178cef68
Kane O'Donnell <kane.odonnell@gmail.com>
2014-01-12 12:13:08 +1100

    Slabgen beta: mostly works!

--- python/slabgen.py
+++ python/slabgen.py
@@ -6,13 +6,19 @@
 # Generates a diamond slab with specified surface orientation and number of layers.
 # Can optionally hydrogen-terminate atoms to make a single-sided slab or a cluster.
 #
-# Usage: slabgen.py [-h -c -d] -n ABC -s X Y Z
+# Usage: slabgen.py -s -p -d -o -z -c -f
 #
-# Options: -h hydrogen-terminates everything that isn't The Surface or periodic wall.
-# -c specifies a cluster. -d specifies double-sided.
+# Nothing is mandatory. By default generates a 10Ax10A (001)-oriented periodic
+# slab. -s is the surface orientation (three integers e.g. 0 0 1), -p is a
+# vector in the surface plane (three floats), -d specifies the dimensions of the 
+# a and b vectors (c is perpendicular to surface) and will not be exactly adhered
+# to. -o specifies a vector offset for all atoms in angstroms. -z is the thickness
+# of the slab, -c specifies a cluster. -f writes a separate CASTEP .cell file
+# with atomic constraints on all terminated atoms and their associated hydrogen.
 #
-# Essentials: -n is the surface orientation and -s is the slab size in multiples of the
-# unit cell parameter. 
+# If a cluster is generated the output is an xyz file and all surface atoms are 
+# hydrogen-terminated. Otherwise there is no hydrogen termination and the output
+# is an XSF.  
 #
 ################################################################################
 #
@@ -35,7 +41,10 @@
 # 
 # NOTES
 #
-# 1. 
+# 1. In principle, any crystal structure and orientation is supported. However,
+# there is some species-dependent infrastructure that has not been finished and
+# parts of the code are "diamond-only". For now that means you can change the 
+# "a" parameter to give diamond, silicon or germanium but that's it.
 #
 ################################################################################
 
@@ -49,11 +58,19 @@ import sys
 
 # Constants
 TOL = 1.0e-6  # Two vectors are the same if the norm of their difference is less than this.
+INT_TOL = 6  # Number of decimal places of agreement required if two rounded floats are
+            # to be considered the same.
 
 # Euclidean Vectors
 Ex = array([1.0, 0.0, 0.0])
 Ey = array([0.0, 1.0, 0.0])
 Ez = array([0.0, 0.0, 1.0])
+
+################################################################################
+#
+# CRYSTAL INFORMATION - DEFAULT IS DIAMOND, MODIFY AT YOUR OWN RISK!
+#
+################################################################################
 
 # Lattice vectors
 # (in conventional unit cell!)
@@ -67,6 +84,35 @@ Px = 0.5 * Lx + 0.5 * Ly
 Py = 0.5 * Ly + 0.5 * Lz
 Pz = 0.5 * Lx + 0.5 * Lz
 
+# Atomic basis (8 atoms)
+basis = array([[0.0,0.0,0.0], [0.5, 0.5, 0.0], [0.5, 0.0, 0.5], [0.0, 0.5, 0.5], \
+               [0.25, 0.25, 0.25], [0.75, 0.75, 0.25], [0.75, 0.25, 0.75], \
+               [0.25, 0.75, 0.75]])
+basis_species = ["C"] * 8
+valence = {'C' : 4}
+
+# Termination parameter (length of X-H bonds).
+termination_bond = {'C' : 1.09}
+
+# Atomic symmetries
+# These are the symmetries not of the crystal but of each unique atomic site. Used to 
+# hydrogen-terminate the slabs.
+### Diamond: each atomic site is sp3 coordinated. From any given bond v, the other three
+### vectors are T(1)v, T(2)v and T(2)T(1)v:
+T1 = matrix([[-1, 0, 0], [0, -1, 0], [0, 0, 1]])
+T2 = matrix([[1, 0, 0], [0, -1, 0], [0, 0, -1]])
+atom_symmetries = {'C': [T1, T2, dot(T1, T2)]}
+
+################################################################################
+#
+# END OF CRYSTAL INFORMATION
+#
+################################################################################
+
+# Should do sanity checks on the crystal parameters here...
+
+# TODO!
+
 # Because the lattice vectors are not necessarily orthogonal we need an overlap matrix
 # (more specifically, the inverse)
 
@@ -77,40 +123,66 @@ Lov = matrix([[dot(Lx, Lx), dot(Lx, Ly), dot(Lx, Lz)],
 Pov = matrix([[dot(Px, Px), dot(Px, Py), dot(Px, Pz)],
               [dot(Py, Px), dot(Py, Py), dot(Py, Pz)],
               [dot(Pz, Px), dot(Pz, Py), dot(Pz, Pz)]])
-
-# Atomic basis (8 atoms)
-basis = array([[0.0,0.0,0.0], [0.5, 0.5, 0.0], [0.5, 0.0, 0.5], [0.0, 0.5, 0.5], \
-               [0.25, 0.25, 0.25], [0.75, 0.75, 0.25], [0.75, 0.25, 0.75], \
-               [0.25, 0.75, 0.75]])
-species = ["C"] * 8
-valence = [4] * 8
-
+              
 # Functions
+def checkSymmetries(vectors, species):
+  """ checkSymmetries(vectors, species)
+  
+  Applies atom_symmetries to the passed vectors. If a vector outside the original set
+  is generated, it's added to a list and returned.
+  
+  """
+  
+  new_vectors = []
+  for v in vectors:
+    for s in atom_symmetries[species]:
+      t = dot(s, v)
+      if not any([(norm(t-x) < TOL).all() for x in vectors]):
+        new_vectors.append(t)
+        
+  return uniqueVectors(new_vectors)
+  
 def radialDistance(vectors, index):
   """ radialDistance(vectors, index)
   
   """
   
   norms = []
+  displacements = []
   for i, v in enumerate(vectors):
     if i != index:
+      displacements.append(v - vectors[index])
       norms.append(norm(v - vectors[index]))
   
-  return norms
+  return norms, displacements
   
-def uniqueFloats(floats, precision):
+def uniqueFloats(floats, vectors):
   """ returns a dict of the unique floats where two floats are the same if they are the 
-  same after rounding to the given number of decimal places.
+  same after rounding to the given number of decimal places. The value of the key-value
+  pair is a list of displacement vectors.
+  
   """
   unique_floats = {}
-  rounded = around(floats, decimals=precision)
-  for f in rounded:
+  rounded = around(floats, decimals=INT_TOL)
+  for f, v in zip(rounded, vectors):
     if f in unique_floats.keys():
-      unique_floats[f] += 1
+      unique_floats[f].append(v)
     else: 
-      unique_floats[f] = 1
+      unique_floats[f] = [v]
   
   return unique_floats
+
+def uniqueVectors(vectors):
+  """ Self-explanatory!
+  
+  """
+  
+  unique_vectors = []
+  for v in vectors:
+    if not any([(norm(v-x) < TOL).all() for x in unique_vectors]):
+      unique_vectors.append(v)
+  
+  return unique_vectors
   
 def normsort(vectors):
   """ normsort(vectors)
@@ -155,6 +227,7 @@ if __name__ == '__main__':
   parser.add_argument("--offset", "-o", type=float, nargs=3, default=[0.0, 0.0, 0.0], help="Add an offset to the atomic positions. Make this small relative to the unit cell dimensions or all hell will break loose.")
   parser.add_argument("--thickness", "-z", type=float, default=0.0, help="Thickness of slab in angstrom. If not set, the length of the surface orientation vector is used.")
   parser.add_argument("--cluster", "-c", action="store_true", help="If set, all unsaturated atoms are hydrogen-terminated and no periodicity checking is done.")
+  parser.add_argument("--fix", "-f", action="store_true", help="If set, writes a .cell file containing constraint information for the terminated atoms. Only works if cluster requested.")
   #parser.add_argument('Z', help="El")
   #parser.add_argument('n', type=int, help="Make the nth atom of element Z special. 1-based.")
   args = parser.parse_args()
@@ -163,7 +236,8 @@ if __name__ == '__main__':
   p = array(args.inplane)
   cell_size = array(args.dimensions)
   is_cluster = args.cluster
-  Voff = array(args.offset)
+  offset_coefficients = array(args.offset)
+  write_fix_file = args.fix
     
   
   # Generate the surface normal vector and the in-plane vector. Check the in-plane 
@@ -229,6 +303,10 @@ if __name__ == '__main__':
   ny = norm(Sy)
   nz = norm(Sz)
   
+  # Expand the offset coefficients in terms of the Si basis.
+  Voff = offset_coefficients[0] * Sx + offset_coefficients[1] * Sy + \
+        offset_coefficients[2] * Sz
+  
   # Cell central point
   Sc = 0.5 * Sx + 0.5 * Sy + 0.5 * Sz
   
@@ -242,14 +320,8 @@ if __name__ == '__main__':
   minz = floor(min(dot(Sx, Ez)/norm(Lz), dot(Sy, Ez)/norm(Lz), dot(Sz, Ez)/norm(Lz))) - 1
   maxz = ceil(max(dot(Sx, Ez)/norm(Lz), dot(Sy, Ez)/norm(Lz), dot(Sz, Ez)/norm(Lz))) + 1
   
-  # Give some output
-  print "Actual lattice: ", Sx, Sy, Sz
-  print ""
-  print "Min and max i: ", minx, maxx
-  print "Min and max j: ", miny, maxy
-  print "Min and max k: ", minz, maxz
-  
   positions = []
+  species = []
   for i in arange(minx, maxx+1):
     for j in arange(miny, maxy+1):
       for k in arange(minz, maxz+1):
@@ -257,7 +329,7 @@ if __name__ == '__main__':
         Vd = i * Lx + j * Ly + k * Lz
         # For each atomic coordinate A in the basis, see if Vd + A is inside the 
         # rhombohedron defined by the cell basis.
-        for b in basis:
+        for b, bs in zip(basis, basis_species):
           tmp = b[0] * Lx + b[1] * Ly + b[2] * Lz + + Voff + Vd
           ax = abs(dot(Sc - tmp, Ux))
           ay = abs(dot(Sc - tmp, Uy))
@@ -266,6 +338,7 @@ if __name__ == '__main__':
           # sort these out later.
           if ax - 0.5 * nx <= TOL and ay - 0.5 * ny <= TOL and az - 0.5 * nz <= TOL:
               positions.append(tmp)
+              species.append(bs)
   
   # We sort the positions by distance from the origin. This is to make the uniqify routine
   # (which happens next) always drop the farthest lattice duplicates rather than the
@@ -301,26 +374,37 @@ if __name__ == '__main__':
       unique_positions.append(positions[i])
  
   # If we want to terminate, do it here.
-  # Not done yet!
-  
-  floats = radialDistance(unique_positions, 35)
-  ufloats = uniqueFloats(floats, 4)
-  for k in sorted(ufloats.keys()):
-    print "%.4g %d" % (k, ufloats[k])
+  if is_cluster:
+    term_positions = []
+    terminated_indices = []
+    for i,p in enumerate(unique_positions):
+      floats, displacements = radialDistance(unique_positions, i)
+      ufloats = uniqueFloats(floats, displacements)
+      nearest_neighbours = ufloats[min(ufloats.keys())]
+      for j, n in enumerate(nearest_neighbours):
+        nearest_neighbours[j] = n / norm(n)
+        
+      if len(nearest_neighbours) < valence['C']:  # need to make this general
+        terminated_indices.append(i)
+        missing = checkSymmetries(nearest_neighbours, 'C') # again, need to generalize
+        for m in missing:
+          # Add a X-H bond.
+          t = array(p + termination_bond['C'] * m / norm(m)).flatten()
+          term_positions.append(t)
     
-  #for p in unique_positions:
-  #  print norm(p)
-  
+            
   # Write a quick output file. If cluster, XYZ, if not XSF.
   if is_cluster:
-    f = open("test.xyz", 'w')
-    f.write(str(len(unique_positions)) + "\n")
+    f = open("new_cluster.xyz", 'w')
+    f.write(str(len(unique_positions)+len(term_positions)) + "\n")
     f.write("TEST OUTPUT\n")
     for p in unique_positions:
       f.write("C    %.6g    %.6g    %.6g\n" % (p[0], p[1], p[2]))
+    for t in term_positions:
+      f.write("H    %.6g    %.6g    %.6g\n" % (t[0], t[1], t[2]))
     f.close()
   else:
-    f = open("test.xsf", 'w')
+    f = open("new_crystal.xsf", 'w')
     f.write("CRYSTAL\n")
     f.write("PRIMVEC\n")
     for v in [Sx, Sy, Sz]:
@@ -332,4 +416,17 @@ if __name__ == '__main__':
     f.write("%d    1\n" % (len(unique_positions)))  
     for p in unique_positions:
       f.write("6    %.6g    %.6g    %.6g\n" % (p[0], p[1], p[2]))
+    f.close()
+  
+  # Write FHI-aims geometry.in file if cluster, along with constraints.
+  if is_cluster:
+    f = open("geometry.in", 'w')
+    f.write("# Written by slabgen.py.\n")
+    for i, p in enumerate(unique_positions):
+      f.write("atom    %.6g    %.6g    %.6g    C\n" % (p[0], p[1], p[2]))
+      if i in terminated_indices:
+        f.write("  constrain_relaxation .true.\n")
+    for t in term_positions:
+      f.write("atom    %.6g    %.6g    %.6g    H\n" % (t[0], t[1], t[2]))
+      f.write("  constrain_relaxation .true.\n")
     f.close()
